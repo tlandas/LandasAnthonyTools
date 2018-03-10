3d261277280dcbde62dfaa64cdd4eb1d5127f2e4
landa057@umn.edu <landa057@umn.edu>
2018-03-10 03:37:58 -0600

    Adjustments to Homework 3

--- Desktop/1st R Pack/LATools/R/Funcs.R
+++ Desktop/1st R Pack/LATools/R/Funcs.R
@@ -1,4 +1,4 @@
- 
+
 #' Log Likelihood
 #'
 #' @param x is data
@@ -7,8 +7,9 @@
 #'
 #' Calculates the log likelihood of a datset over a certain distribution and interval determined by the user
 #' @export
-#' @examples  
+#' @examples
 #' data(d)
+#' binomial = function(theta, x) {dbinom(x, 20, prob = 1 / (1 + exp(- theta)), log = TRUE)}
 #' logL(d, binomial, c(-100, 100))
 
 logL <-function(x, fun, int)
@@ -28,18 +29,33 @@ logL <-function(x, fun, int)
 #' @param xcol is the number of columns in x
 #' @param ycol is the number of columns in y
 #' @param colorVar is the color of the data points in the plot
-#' 
+#'
 #' Creates a graph
 #' @export
-#' @examples 
+#' @examples
 #' data(flights)
-#' ggplotW(flights, 2, 2, color = blue)
+#' ggplotW(flights, "dep_time", "dep_delay", color = blue)
 ggplotW <- function(data, xcol, ycol, colorVar)
 {
   plot <- ggplot(data=data, aes(x = xcol, y = ycol, color = colorVar)) + geom_point()
-  print(plot)
   return(plot)
-} 
+}
+
+#' Matrix Adjuster
+#'
+#' @param data is data
+#' @param colName is the column the user wants, in quotes
+#'
+#' Uses dpylr on a matrix
+#' @export
+#' @examples
+#' data(flights)
+#' adjMat(flights, "dep_time")
+adjMat <- function(data, colName)
+{
+  column = dpylr::select(data, dpylr::contains(colName))
+  return(column)
+}
 
 #' Measures of Spread
 #'
@@ -47,15 +63,15 @@ ggplotW <- function(data, xcol, ycol, colorVar)
 #'
 #' Gives the mean, variance and standard deviation of a population from data, x.
 #' @export
-#' @examples 
+#' @examples
 #' data(d)
 #' spread(d)
 spread <- function(d)
 {
   moo = sum(d$x* d$p)
-  
+
   sig2 = (sum(d$x - moo)^2) * d$p
-  
+
   sD = sqrt(sig2)
 }
 
@@ -65,8 +81,8 @@ spread <- function(d)
 #'
 #' Gives the mean, variance and standard deviation of a population from data, x in the form of a list. Has checks implemented to ensure the data given is proper
 #' @export
-#' @examples 
-#' data(d) 
+#' @examples
+#' data(d)
 #' spread(d)
 spreadCheck <- function(d)
 {
@@ -77,13 +93,13 @@ spreadCheck <- function(d)
   stopifnot(is.finite(d$p))
   stopifnot(is.numeric(d$p))
   stopifnot(all.equal(1, sum(d$p)))
-  
+
   moo = sum(d$x * d$p)
-  
+
   sig2 = sum(((d$x - moo)^2) * d$p)
-  
+
   sD = (sqrt(sig2))
-  
+
   list(mu = moo, variance = sig2, StandarDev = sD)
 }
 
@@ -92,18 +108,18 @@ spreadCheck <- function(d)
 #' @param x is matrix
 #' @param MARGIN is the dimiensions of the matrix.
 #' @param FUN is a predefined function.
-#' 
+#'
 #' Returns a simplified array similar to other apply functiion already in R.
 #' @export
-#' @examples 
-#' data(e) 
-#' myapply(e, 1, mean)
+#' @examples
+#' data(d)
+#' myapply(d, 1, mean)
 myapply <- function(X, MARGIN, FUN, ...)
 {
   R = dim(X)[1]
   C = dim(X)[2]
   f = match.fun(FUN)
-  
+
   if (MARGIN == 1)
   {
     result = list()
